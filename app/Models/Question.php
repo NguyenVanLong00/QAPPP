a1265af5acfe5e3f14e60e6f618304f6e54bd4cf
Nguyen Van Long <nguyenvanlong0x0@gmail.com>
2021-08-02 10:59:53 +0700

    add seed user and quetion using model factory

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -21,6 +21,5 @@ class Question extends Model
     public function setTitleAttribute($value){
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = Str::slug($value);
-
     }  
 }
